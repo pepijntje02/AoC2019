c2b2834534b2fffcd77d4a4f4bb7981fa82cc57d
pepijntje02 <pepijn13@msn.com>
2019-12-06 14:05:44 +0100

    day 6

--- day6/day6.py
+++ day6/day6.py
@@ -5,19 +5,17 @@ def readPuzzle(fname):
 
 def pathFinding(path, puzzle, start, value):
     path[start] = value
-    nex = [puzzle[1][i] for i,x in enumerate(puzzle[0]) if x == start]
+    nex = [puzzle[1][i] for i, x in enumerate(puzzle[0]) if x == start]
     if nex:
         for n in nex:
             pathFinding(path, puzzle, n, value+1)
 
 def part1(puzzle):
-    puzzle = list(map(list, zip(*puzzle)))
     path = dict()
     pathFinding(path, puzzle, 'COM', 0)
     print('Total number of direct and indirect orbits: ', sum(path.values()))
 
 def part2(puzzle):
-    puzzle = list(map(list, zip(*puzzle)))
     pathYou = findPath("YOU", puzzle)
     pathSanta = findPath("SAN", puzzle)
     orbitTransfer = len(set(pathYou) - set(pathSanta)) + len(set(pathSanta) - set(pathYou)) - 2
@@ -32,5 +30,6 @@ def findPath(name, puzzle):
 if __name__ == '__main__':
     fname = 'input.txt'
     puzzle = readPuzzle(fname)
+    puzzle = list(map(list, zip(*puzzle))) # Transpose list ([nx2] to [2xn])
     part1(puzzle[:])
     part2(puzzle[:])
